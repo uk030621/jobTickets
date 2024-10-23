c52d87e1f92df91c3109ac11d9d5ff9826114dc6
uk030621 <123797901+uk030621@users.noreply.github.com>
2024-10-23 21:56:09 +0100

    Fixed ticket display text not wrapping issue.

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -25,7 +25,7 @@ const TicketCard = ({ ticket }) => {
   const createdDateTime = formatTimestamp(ticket.createdAt);
 
   return (
-    <div className="flex flex-col hover:bg-card-hover bg-card rounded-md shadow-lg p-3 m-2">
+    <div className="flex flex-col hover:bg-card-hover bg-card rounded-md shadow-lg p-3 m-2 w-full max-w-full">
       <div className="flex mb-3">
         <PriorityDisplay priority={ticket.priority} />
         <div className="ml-auto">
@@ -34,16 +34,18 @@ const TicketCard = ({ ticket }) => {
       </div>
       <Link href={`/TicketPage/${ticket._id}`} style={{ display: "contents" }}>
         <h4 className="mb-1">{ticket.title}</h4>
-        <hr className="h-px  border-0 bg-page mb-2 "></hr>
-        <p className="whitespace-pre-wrap">{ticket.description}</p>
+        <hr className="h-px border-0 bg-page mb-2" />
+        <p className="whitespace-pre-wrap break-words overflow-hidden text-sm">
+          {ticket.description}
+        </p>
 
         <div className="flex-grow"></div>
         <div className="flex mt-2">
           <div className="flex flex-col">
-            <p className="text-xs  my-1">{createdDateTime}</p>
+            <p className="text-xs my-1">{createdDateTime}</p>
             <ProgressDisplay progress={ticket.progress} />
           </div>
-          <div className="ml-auto  flex items-end">
+          <div className="ml-auto flex items-end">
             <StatusDisplay status={ticket.status} />
           </div>
         </div>
